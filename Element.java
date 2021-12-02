cbd0a1d174a4fe57af9b62655e3720643e97e3b5
HerrRoth <93282643+HerrRoth@users.noreply.github.com>
2021-12-02 20:31:13 +0100

    Lehrer-Edit 1

--- Element.java
+++ Element.java
@@ -1,4 +1,24 @@
 public class Element{
     public int wert;
     public Element nachfolger;
+
+    public Element(int i){
+        wert = i;
+    }
+
+    public void setWert(int i){
+        wert = i;
+    }
+
+    public void setNachfolger(Element e){
+        nachfolger = e;
+    }
+
+    public int getWert{
+        return wert;
+    }
+
+    public Element getNachfolger{
+        return nachfolger;
+    }
 }
--- Stack.java
+++ Stack.java
@@ -1,22 +1,37 @@
 public class Stack{
 
+    private Element oberstesElement;
+    int hoehe = 0;
+
     public Stack(){
 
     }
     
     public boolean isEmpty(){
-        return true;
+       if (oberstesElement == null){
+           return true;
+       }
+       return false;
     }
     
+    public void push(Element e){
+        e.setNachfolger(oberstesElement);
+        oberstesElement = e;
+    }
+
     public void push(int i){
-        
+        Element e = new Element(i);
+        //Element e = new Element();
+        //e.wert = i;
+        e.setNachfolger(oberstesElement);
+        oberstesElement = e;
     }
     
     public void pop(){
-        
+        oberstesElement = oberstesElement.getNachfolger();
     }
     
     public int top(){
-        return 0;
+        return oberstesElement;
     }
 }
