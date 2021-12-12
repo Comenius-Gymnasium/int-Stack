6e3c5598b1388d94223088a8ee9c2c4838f236c1
HerrRoth <93282643+HerrRoth@users.noreply.github.com>
2021-12-12 22:53:19 +0100

    Mehr Kommentare

--- Stack.java
+++ Stack.java
@@ -7,6 +7,7 @@ public class Stack{
 
     }
     
+    //gibt zurück ob der Stack leer ist
     public boolean isEmpty(){
        if (oberstesElement == null){
            return true;
@@ -14,6 +15,7 @@ public class Stack{
        return false;
     }
     
+    //legt ein als Parameter übergebenes Element oben auf den Stack
     public void push(Element e){
         e.setNachfolger(oberstesElement);
         oberstesElement = e;
@@ -21,24 +23,30 @@ public class Stack{
         //hoehe = hoehe + 1;
     }
 
+    //erzeugt ein neues Element, weist es den als Parameter übergebenen int als neuen wert zu und legt es oben auf den Stack
     public void push(int i){
         Element e = new Element(i);
-        //Element e = new Element();
-        //e.wert = i;
+            //Man könnte es auch in zwei Schritten machen:
+            //Element e = new Element();
+            //e.wert = i; oder e.setWert(i);
         e.setNachfolger(oberstesElement);
         oberstesElement = e;
     }
     
+    //entfernt das oberste Element
     public void pop(){
         oberstesElement = oberstesElement.getNachfolger();
         hoehe--;
     }
     
+    //gibt das oberste Element zurück
     public Element topElement(){
         return oberstesElement;
     }
     
+    //gibt den wert des obersten Elementes zurück
     public int top(){
         return oberstesElement.wert;
+        //oder return oberstesElement.getWert();
     }
 }
